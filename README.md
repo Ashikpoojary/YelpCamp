734dbcc7a9bf05cdfb512b268edea780b2ed4ca0
Ashikpoojary <ashikpoojary06@gmail.com>
2024-03-01 22:17:00 +0530

    Adding-Basic-Style

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 You need to install : node, ejs, mongoose
+
+
+To delete & create new database collection just run "node seeds/index.js" & restart mongosh db again
--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async() =>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random()*1000); //create random set of array for Title
+        const price = Math.floor(Math.random()*20) +10;
         const camp = new Campground({
             location:`${cities[random1000].city},${cities[random1000].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/random/?camping',
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, adipisci sunt. Pariatur totam temporibus fugiat sint eaque cupiditate dolores sequi atque consequatur assumenda impedit quasi voluptas harum, maxime rerum suscipit?",
+            price 
         })
         await camp.save();
     }
