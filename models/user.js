1fe5eda53a1f13e8a95a0e6c9ae6c45fc86d1b52
Ashikpoojary <ashikpoojary06@gmail.com>
2024-03-10 16:56:52 +0530

    Adding-Authentication

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose); //Make sure the username is Unique
+
+module.exports = mongoose.model('User', UserSchema);
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,7 @@ const catchAsync = require('../utils/catchAsync');
 const {campgroundSchema} = require('../schemas.js');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
+const {isLoggedIn} = require('../middleware');
 
 
 const validateCampground = (req,res,next) =>{
@@ -22,11 +23,11 @@ router.get('/',catchAsync(async(req,res)=>{
     res.render('campgrounds/index',{campgrounds})
 }));
 
-router.get('/new',(req,res)=>{
+router.get('/new',isLoggedIn,(req,res)=>{
     res.render('campgrounds/new');
 });
 
-router.post('/',validateCampground,catchAsync(async(req,res,next)=>{
+router.post('/',isLoggedIn,validateCampground,catchAsync(async(req,res,next)=>{
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
     
     const campground = new Campground(req.body.campground);
@@ -44,7 +45,7 @@ router.get('/:id',catchAsync(async(req,res)=>{
     res.render('campgrounds/show', {campground});
 }));
 
-router.get('/:id/edit',catchAsync(async(req,res)=>{
+router.get('/:id/edit',isLoggedIn,catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id)
     if(!campground){
         req.flash('error','Cannot find that campground');
@@ -53,7 +54,7 @@ router.get('/:id/edit',catchAsync(async(req,res)=>{
     res.render('campgrounds/edit', {campground});
 }));
 
-router.put('/:id',validateCampground,catchAsync(async(req,res)=>{
+router.put('/:id',isLoggedIn,validateCampground,catchAsync(async(req,res)=>{
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id,{ ...req.body.campground},{new:true});
     req.flash('success','Successfully updated campground!')
